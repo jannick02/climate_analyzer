e423b8a5e3fee245a360a28836da9cbc08e05b61
jannick02 <jannick02.js@gmail.com>
2026-05-15 12:36:20 +0200

    Add new constants for ideal climate values

--- custom_components/climate_analyzer/const.py
+++ custom_components/climate_analyzer/const.py
@@ -1,7 +1,12 @@
 DOMAIN = "climate_analyzer"
+CONF_ROOM_NAME = "room_name"
 CONF_TEMP_IN = "temp_in"
 CONF_HUM_IN = "hum_in"
 CONF_TEMP_OUT = "temp_out"
 CONF_HUM_OUT = "hum_out"
 CONF_WINDOW_SENSOR = "window_sensor"
-CONF_ROOM_NAME = "room_name"
+
+# Neue Konstanten für die konfigurierbaren Idealwerte
+CONF_IDEAL_TEMP = "ideal_temp"
+CONF_IDEAL_ABS_HUM = "ideal_abs_hum"
+CONF_MAX_ABS_HUM = "max_abs_hum"
